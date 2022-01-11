ffc029818dbc08e310a61f15aa53f8407edaf360
Moody Reaper <efrem.sarmazov@gmail.com>
2022-01-11 16:37:36 +0300

    adding waybar config

---
+++ waybar/scripts/dunst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COUNT=$(dunstctl count waiting)
+ENABLED=
+DISABLED=
+if [ $COUNT != 0 ]; then DISABLED=" $COUNT"; fi
+if dunstctl is-paused | grep -q "false";
+then echo '{"text": "'"$ENABLED"'", "tooltip": "Dunst noficitaions are enabled", "class": "custom-enabled"}';
+else echo '{"text": "'"$DISABLED"'", "tooltip": "Dunst noficitaions are disabled", "class": "custom-disabled"}'; fi
---
+++ waybar/scripts/flavours-theme-current.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://github.com/Misterio77/dotfiles/blob/sway/home/.config/waybar/config.json
+
+THEME=$(flavours current)
+echo '{"text": "嗀", "tooltip": "Current theme: '"$THEME"'"}'
