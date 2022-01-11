98f19bea1a577e5a00b9b8f86cdf66017377c543
Moody Reaper <efrem.sarmazov@gmail.com>
2022-01-11 16:34:26 +0300

    adding sway config

---
+++ sway/scripts/flavours-sway-alpha-colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file="$HOME/.config/sway/config"
+
+alphastring="set \$opacity"
+aphasearchresult=$(grep "$alphastring" $file)
+alphavalue=$(cut -d " " -f3 <<< "$aphasearchresult")
+
+colors=(base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F)
+
+for color in ${colors[@]}; do
+    searchstring="set $"$color" #"
+    searchresult=$(grep "$searchstring" $file)
+    colorvalue=$(cut -d "#" -f2 <<< "$searchresult")
+    alphacolorstring="set $"$color-alpha" #$colorvalue$alphavalue"
+    sed -i "s/$searchstring.*/&\n$alphacolorstring/" $file
+done
