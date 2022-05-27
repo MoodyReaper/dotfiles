27b9a4e55a75f3a93fc08c6a4c6e981ccdc297d2
Moody Reaper <efrem.sarmazov@gmail.com>
2022-05-27 11:31:57 +0600

    fish - gnome keyring environment variables fix

--- fish/conf.d/gnome_keyring.fish
+++ fish/conf.d/gnome_keyring.fish
@@ -1,5 +1,11 @@
+# Yes, it is horrible
+# But "set -x (gnome-keyring-daemon --start | string split "=")" mentioned in https://wiki.archlinux.org/title/GNOME/Keyring#Shell
+# doesn't work as intended because it creates only one ENV variable: "GNOME_KEYRING_CONTROL=/run/user/1000/keyring SSH_AUTH_SOCK /run/user/1000/keyring/ssh"
+
 # Run only on login
 if status --is-login
-    # https://wiki.archlinux.org/title/GNOME/Keyring#Shell
-    set -x (gnome-keyring-daemon --start | string split "=")
+    set ENV_VARIABLES (gnome-keyring-daemon --start | string split " ")
+    for ENV_VARIABLE in $ENV_VARIABLES
+        set -x (echo $ENV_VARIABLE | string split "=")
+    end
 end
