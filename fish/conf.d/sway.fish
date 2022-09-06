62c1a9b7a07786cb0cf58cb6ceec1825f18251da
Moody Reaper <efrem.sarmazov@gmail.com>
2022-09-06 12:31:35 +0600

    fish: sway - tty3 launch as a login manager fallback

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -1,15 +1,16 @@
 # Run only on login
 if status --is-login
 
-    # If running from tty1 set Wayland environment variables and start Sway
-    #set TTY1 (tty)
-    #if [ "$TTY1" = /dev/tty1 ]
-    #    # Set Wayland/Sway ENV variables
-    #    env-variables-wayland-sway
-    #    # Sway start
-    #    # https://github.com/swaywm/sway/wiki#login-managers
-    #    exec sway
-    #end
+    # If running from tty3 set Wayland environment variables and start Sway
+    # Used as a fallback in case of broken login manager
+    set TTY3 (tty)
+    if [ "$TTY3" = /dev/tty3 ]
+        # Set Wayland/Sway ENV variables
+        env-variables-wayland-sway
+        # Sway start
+        # https://github.com/swaywm/sway/wiki#login-managers
+        exec sway
+    end
 
     # Check if ENV variable exists
     if test -n "$XDG_SESSION_TYPE"
