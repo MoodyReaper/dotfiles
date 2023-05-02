4e8f1c950480cf02b407576d0f5826e7a90e16e9
Moody Reaper <efrem.sarmazov@gmail.com>
2023-05-02 14:56:16 +0600

    sway, waybar: update icons

--- waybar/scripts/flavours-theme-current.sh
+++ waybar/scripts/flavours-theme-current.sh
@@ -4,4 +4,4 @@
 # https://github.com/Misterio77/dotfiles/blob/sway/home/.config/waybar/config.json
 
 THEME=$(flavours current)
-echo '{"text": "嗀", "tooltip": "Current theme: '"$THEME"'"}'
+echo '{"text": "", "tooltip": "Current theme: '"$THEME"'"}'
