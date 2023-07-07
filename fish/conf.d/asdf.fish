71249df7ab036079fdf51544322a489d70703a63
Moody Reaper <efrem.sarmazov@gmail.com>
2023-07-07 15:55:15 +0600

    fish: add `asdf` settings for `go` plugin

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -1,2 +1,5 @@
 # https://asdf-vm.com/guide/getting-started.html#_3-install-asdf
 source /opt/asdf-vm/asdf.fish
+
+# https://github.com/kennyp/asdf-golang#goroot
+source ~/.asdf/plugins/golang/set-env.fish
