bd3cde3371ca8e24551f9c0e398e52e35e5dfcac
Moody Reaper <efrem.sarmazov@gmail.com>
2024-12-01 00:16:54 +0500

    chore(fish): add `libva` driver name for hardware acceleration

--- fish/conf.d/hardware_acceleration.fish
+++ fish/conf.d/hardware_acceleration.fish
@@ -1,5 +1,7 @@
 # Run only on login
 if status --is-login
+    # https://wiki.archlinux.org/title/Hardware_video_acceleration#Configuring_VA-API
+    set -gx LIBVA_DRIVER_NAME radeonsi
     # https://wiki.archlinux.org/title/Hardware_video_acceleration#Configuring_VDPAU
     set -gx VDPAU_DRIVER radeonsi
 end
